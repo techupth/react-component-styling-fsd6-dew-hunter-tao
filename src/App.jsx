99743e396b3e39e36cf43e5ea34922fd4d5301df
Surakit <surakitkunkayun@gmail.com>
2024-06-04 15:49:22 +0700

    feat: fininshed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import "./App.css";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+       {<Button color = "primary" text = "Large"/>}
+       {<Button color = "secondary" text = "Large"/>} 
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+        {<Alert status = "error" text = "This is error alert box"/>}
+        {<Alert status = "warning" text = "This is warning alert box"/>}
+        {<Alert status = "info" text = "This is info alert box"/>}
+        {<Alert status = "success" text = "This is success alert box"/>}
       </div>
     </div>
   );
 }
+
+
 
 export default App;
